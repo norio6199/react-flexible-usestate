3bbf2723232ea81b4506e6ac98103f4689710f08
norio6199 <norio6199@gmail.com>
2022-01-03 18:26:48 +0900

    change return type from array to object

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
 import { useState } from 'react';
 import produce from 'immer';
 
-const useStates = <T>(initialValues: T) => {
+type UseStatesReturn<T> = {
+  states: T;
+  setState<K extends keyof T>(key: K, value: T[K] | ((prev: T[K]) => T[K])): void;
+  resetState<K extends keyof T>(key?: K): void;
+};
+
+const useStates = <T>(initialValues: T): UseStatesReturn<T> => {
   const [states, _setStates] = useState(initialValues);
 
   const setStates = (recipe: (draft: T) => void): void => {
     _setStates(prev => produce(prev, recipe));
   };
 
-  const setState = <K extends keyof T>(key: K, value: T[K] | ((prev: T[K]) => T[K])) => {
+  const setState = <K extends keyof T>(key: K, value: T[K] | ((prev: T[K]) => T[K])): void => {
     setStates(draft => {
       draft[key] =
         typeof value === 'function' ? (value as (prev: T[K]) => T[K])(draft[key]) : value;
     });
   };
 
-  const resetState = <K extends keyof T>(key?: K) => {
+  const resetState = <K extends keyof T>(key?: K): void => {
     let targetKeys: (keyof T)[];
     if (!key) {
       targetKeys = Object.keys(initialValues) as (keyof T)[];
@@ -29,7 +35,7 @@ const useStates = <T>(initialValues: T) => {
     });
   };
 
-  return [states, setState, resetState] as [typeof states, typeof setState, typeof resetState];
+  return { states, setState, resetState };
 };
 
 export default useStates;
