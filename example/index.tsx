9673b4c7e776098f3e777f08fa57c34380af6e99
norio6199 <norio6199@gmail.com>
2022-01-03 18:28:03 +0900

    test at example

--- example/index.tsx
+++ example/index.tsx
@@ -6,31 +6,31 @@ import useStates from '../.';
 
 const initialValues = {
   text: 'hogehoge',
-  number: 123
-}
+  number: 123,
+};
 
 const App: React.VFC = () => {
-  const [ states, setStates, resetStates ] = useStates(initialValues);
+  const { states, setState, resetState } = useStates(initialValues);
 
   const handleInclimentText = (): void => {
-    setStates('text', `${states.text} + fuga`)
-  }
+    setState('text', `${states.text} + fuga`);
+  };
   const handleResetText = (): void => {
-    resetStates('text');
-  }
+    resetState('text');
+  };
 
   const handleInclimentNumber = (): void => {
-    setStates('number', (prev) => {
-      return prev * 2
-    })
-  }
+    setState('number', prev => {
+      return prev * 2;
+    });
+  };
   const handleResetNumber = (): void => {
-    resetStates('number');
-  }
+    resetState('number');
+  };
 
   const handleResetAll = (): void => {
-    resetStates();
-  }
+    resetState();
+  };
 
   return (
     <div>
